a268267d8c0d5128110324ecbbe4bc492ce5d619
Raphael Jancherov <bokettodev@gmail.com>
2022-03-04 02:41:12 +0300

    refactor: common improvements

--- src/components/world-map.svelte
+++ src/components/world-map.svelte
@@ -1,50 +1,43 @@
 <script lang="ts">
 	import { draw } from 'svelte/transition';
 	import { quadInOut } from 'svelte/easing';
-	import {
-		json,
-		geoPath,
-		geoMercator,
-		type GeoProjection,
-		type GeoPath,
-		type GeoPermissibleObjects
-	} from 'd3';
+	import { json, geoPath, geoMercator, type GeoProjection, type GeoPath } from 'd3';
 	import { onMount } from 'svelte';
 
 	let geoJsonFeatures: GeoJSON.Feature[] = [];
 	let mercatorProjection: GeoProjection;
-	let pathGenerator: GeoPath<any, GeoPermissibleObjects>;
+	let pathGenerator: GeoPath;
 
 	let windowHeight: number;
 	let windowWidth: number;
 
-	const width = '100%';
-	const height = '100%';
-	const url = './static/world.geojson';
-
-	function loadJson(): void {
-		json(url).then((worldGeoJson: GeoJSON.FeatureCollection): void => {
+	function loadWorldGeoJson(): void {
+		json('./static/world.geojson').then((worldGeoJson: GeoJSON.FeatureCollection): void => {
 			setProjectionAndPath(worldGeoJson);
 			geoJsonFeatures = worldGeoJson.features;
 		});
 	}
 
 	function setProjectionAndPath(geoJson: GeoJSON.FeatureCollection): void {
-		mercatorProjection = geoMercator().fitSize([windowWidth, windowHeight], geoJson);
+		mercatorProjection = geoMercator()
+			.fitSize([windowWidth, windowHeight], geoJson)
+			.rotate([-11, 0]);
 		pathGenerator = geoPath(mercatorProjection);
 	}
 
 	onMount((): void => {
-		loadJson();
+		loadWorldGeoJson();
 	});
 </script>
 
 <svelte:window bind:innerHeight={windowHeight} bind:innerWidth={windowWidth} />
 
-<svg {width} {height}>
+<svg height="100%" width="100%">
 	{#each geoJsonFeatures as geoJsonFeature}
 		<path
 			transition:draw={{ duration: 3000, delay: 0, easing: quadInOut }}
+			data-iso={geoJsonFeature.id}
+			data-name={geoJsonFeature.properties.name}
 			d={pathGenerator(geoJsonFeature)}
 		/>
 	{/each}
@@ -52,7 +45,7 @@
 
 <style>
 	path {
-		fill: none;
-		stroke: darkgreen;
+		fill: darkslategray;
+		stroke: white;
 	}
 </style>
