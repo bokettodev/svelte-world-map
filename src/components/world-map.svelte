6074b50cf2dbff9dffb5d3ef3ed7d1e9bb3f7df4
Raphael Jancherov <bokettodev@gmail.com>
2022-03-11 01:19:27 +0300

    chore: mercator view removed

--- src/components/world-map.svelte
+++ src/components/world-map.svelte
@@ -2,12 +2,10 @@
 	import { onMount } from 'svelte';
 	import { json } from 'd3';
 	import * as topojson from 'topojson-client';
-	import WorldMapMercatorSvg from './world-map-mercator-svg.svelte';
 	import WorldMapOrthographic from './world-map-orthographic.svelte';
 	import { WorldDataset } from '../classes/world-data.class';
 	import type { TopoJson } from '../types/topo-json.type';
 
-	let mercator = false;
 	let width: number;
 	let height: number;
 	const worldDataset = new WorldDataset();
@@ -31,25 +29,12 @@
 </script>
 
 <div bind:clientWidth={width} bind:clientHeight={height}>
-	<input type="checkbox" bind:checked={mercator} />
-
-	{#if mercator}
-		<WorldMapMercatorSvg />
-	{:else}
-		<WorldMapOrthographic {worldDataset} {width} {height} />
-	{/if}
+	<WorldMapOrthographic {worldDataset} {width} {height} />
 </div>
 
 <style lang="scss">
 	div {
-		position: relative;
 		height: 100%;
 		width: 100%;
-
-		input {
-			position: absolute;
-			top: 20px;
-			left: 20px;
-		}
 	}
 </style>
