e0526f7ca36fe01d00d1e0e6d0970f8c136b4acb
Raphael Jancherov <bokettodev@gmail.com>
2022-03-19 23:14:33 +0300

    feat: prepared for gh-pages

---
+++ docs/_app/pages/__layout.svelte-bb7e9b03.js
@@ -0,0 +1,59 @@
+import{S as f,i as u,s as m,F as h,e as g,t as _,k as b,G as y,c as v,a as w,h as x,d as c,m as $,b as z,H as p,g as R,I as k,J as q,K as E,q as H,o as S}from"../chunks/vendor-9fac88bb.js";function A(s){let e,i,l,o;const r=s[1].default,t=h(r,s,s[0],null);return{c(){e=g("style"),i=_(`@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+:root {
+  --bg-global-color: white;
+  --bg-element-color: white;
+  --primary-color: transparent;
+  --secondary-color: transparent;
+  --tertiary-color: transparent;
+  --accent-color: transparent;
+  --default-indent: 12px;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+*:focus {
+  outline: none;
+}
+
+html {
+  font-size: 16px;
+}
+
+body {
+  background-color: var(--bg-global-color);
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  height: 100vh;
+  width: 100vw;
+}`),l=b(),t&&t.c(),this.h()},l(n){const a=y('[data-svelte="svelte-nm9x7n"]',document.head);e=v(a,"STYLE",{lang:!0});var d=w(e);i=x(d,`@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+:root {
+  --bg-global-color: white;
+  --bg-element-color: white;
+  --primary-color: transparent;
+  --secondary-color: transparent;
+  --tertiary-color: transparent;
+  --accent-color: transparent;
+  --default-indent: 12px;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+*:focus {
+  outline: none;
+}
+
+html {
+  font-size: 16px;
+}
+
+body {
+  background-color: var(--bg-global-color);
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  height: 100vh;
+  width: 100vw;
+}`),d.forEach(c),a.forEach(c),l=$(n),t&&t.l(n),this.h()},h(){document.title="World Map Interactive",z(e,"lang","scss")},m(n,a){p(document.head,e),p(e,i),R(n,l,a),t&&t.m(n,a),o=!0},p(n,[a]){t&&t.p&&(!o||a&1)&&k(t,r,n,n[0],o?E(r,n[0],a,null):q(n[0]),null)},i(n){o||(H(t,n),o=!0)},o(n){S(t,n),o=!1},d(n){c(e),n&&c(l),t&&t.d(n)}}}function I(s,e,i){let{$$slots:l={},$$scope:o}=e;return s.$$set=r=>{"$$scope"in r&&i(0,o=r.$$scope)},[o,l]}class F extends f{constructor(e){super();u(this,e,I,A,m,{})}}export{F as default};
