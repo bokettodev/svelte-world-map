1aa07a4b9eda26efbc633bd43dd755b8ba00be6f
Raphael Jancherov <bokettodev@gmail.com>
2022-03-20 00:00:47 +0300

    fix: svelte.config base path corrected

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
@@ -11,6 +13,9 @@ const config = {
 			fallback: null,
 			precompress: false
 		}),
+		paths: {
+			base: dev ? '' : '/svelte-world-map'
+		},
 		prerender: {
 			default: true
 		}
