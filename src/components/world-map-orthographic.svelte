a03262ebe7289687e63ff06301027028cdc82ca7
Raphael Jancherov <bokettodev@gmail.com>
2022-03-28 23:18:04 +0300

    feat: base color pickers added

--- src/components/world-map-orthographic.svelte
+++ src/components/world-map-orthographic.svelte
@@ -9,14 +9,20 @@
 	export let height: number;
 
 	const orthographic = new Orthographic();
+
 	let canvas: HTMLCanvasElement;
+	let sphereColor = '#2f4f4f';
+	let waterColor = '#ffffff';
+	let boundariesColor = '#ffffff';
+	let earthColor = '#2f4f4f';
+	let hoverColor = '#3e9987';
 
 	let orthographicInited = false;
 	let mapInited = false;
 	const resizeDebounceMs = 300;
 
 	const onResize = debounce(() => {
-		orthographic.drawMap();
+		orthographic.drawMapWithCalculations();
 	}, resizeDebounceMs);
 
 	beforeUpdate((): void => {
@@ -39,10 +45,87 @@
 
 		mapInited = true;
 		orthographic.setWorldDataset(worldDataset);
-		orthographic.drawMap();
+
+		orthographic.setSphereColor(sphereColor);
+		orthographic.setWaterColor(waterColor);
+		orthographic.setBoundariesColor(boundariesColor);
+		orthographic.setEarthColor(earthColor);
+		orthographic.setHoverColor(hoverColor);
+
+		orthographic.drawMapWithCalculations();
 	}
+
+	const onSphereColorChange = (): void => {
+		orthographic.setSphereColor(sphereColor);
+		orthographic.drawMapWithoutCalculations();
+	};
+
+	const onWaterColorChange = (): void => {
+		orthographic.setWaterColor(waterColor);
+		orthographic.drawMapWithoutCalculations();
+	};
+
+	const onBoundariesColorChange = (): void => {
+		orthographic.setBoundariesColor(boundariesColor);
+		orthographic.drawMapWithoutCalculations();
+	};
+
+	const onEarthColorChange = (): void => {
+		orthographic.setEarthColor(earthColor);
+		orthographic.drawMapWithoutCalculations();
+	};
+
+	const onHoverColorChange = (): void => {
+		orthographic.setHoverColor(hoverColor);
+		orthographic.drawMapWithoutCalculations();
+	};
 </script>
 
 <svelte:window on:resize={onResize} />
 
+<div class="colors">
+	<div class="color">
+		<p>Sphere</p>
+		<input type="color" bind:value={sphereColor} on:change={onSphereColorChange} />
+	</div>
+
+	<div class="color">
+		<p>Water</p>
+		<input type="color" bind:value={waterColor} on:change={onWaterColorChange} />
+	</div>
+
+	<div class="color">
+		<p>Boundaries</p>
+		<input type="color" bind:value={boundariesColor} on:change={onBoundariesColorChange} />
+	</div>
+
+	<div class="color">
+		<p>Earth</p>
+		<input type="color" bind:value={earthColor} on:change={onEarthColorChange} />
+	</div>
+
+	<div class="color">
+		<p>Hover</p>
+		<input type="color" bind:value={hoverColor} on:change={onHoverColorChange} />
+	</div>
+</div>
+
 <canvas bind:this={canvas} {width} {height} />
+
+<style lang="scss">
+	.colors {
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		top: 1.25rem;
+		right: 1.25rem;
+		gap: 0.75rem;
+	}
+
+	.color {
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		gap: 0.75rem;
+	}
+</style>
