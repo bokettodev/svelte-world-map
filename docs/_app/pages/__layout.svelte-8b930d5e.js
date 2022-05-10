448415ad61db12ad690686bf94164e5f997976a3
Raphael Jancherov <bokettodev@gmail.com>
2022-05-10 18:53:57 +0300

    chore: static rebuild

---
+++ docs/_app/pages/__layout.svelte-8b930d5e.js
@@ -0,0 +1,29 @@
+import{S as u,i as m,s as p,F as _,e as h,t as g,k as y,G as b,c as v,a as $,h as w,d as c,m as R,b as q,H as f,g as x,I as E,J as H,K as S,q as k,o as z}from"../chunks/vendor-6312896e.js";function A(o){let s,r,l,n;const i=o[1].default,e=_(i,o,o[0],null);return{c(){s=h("style"),r=g(`@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+*:focus {
+  outline: none;
+}
+
+body {
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  height: 100vh;
+  width: 100vw;
+}`),l=y(),e&&e.c(),this.h()},l(t){const a=b('[data-svelte="svelte-1n3ikre"]',document.head);s=v(a,"STYLE",{lang:!0});var d=$(s);r=w(d,`@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+*:focus {
+  outline: none;
+}
+
+body {
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  height: 100vh;
+  width: 100vw;
+}`),d.forEach(c),a.forEach(c),l=R(t),e&&e.l(t),this.h()},h(){document.title="World Map Interactive",q(s,"lang","scss")},m(t,a){f(document.head,s),f(s,r),x(t,l,a),e&&e.m(t,a),n=!0},p(t,[a]){e&&e.p&&(!n||a&1)&&E(e,i,t,t[0],n?S(i,t[0],a,null):H(t[0]),null)},i(t){n||(k(e,t),n=!0)},o(t){z(e,t),n=!1},d(t){c(s),t&&c(l),e&&e.d(t)}}}function I(o,s,r){let{$$slots:l={},$$scope:n}=s;return o.$$set=i=>{"$$scope"in i&&r(0,n=i.$$scope)},[n,l]}class F extends u{constructor(s){super();m(this,s,I,A,p,{})}}export{F as default};
